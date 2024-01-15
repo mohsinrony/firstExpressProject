c798b53c984460735fc323d7c449bd335e597c7f
[mdmohsin] <[roni07wd@gmail.com]>
2024-01-15 10:50:42 +0200

    add index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+
+const port = 3000;
+const host = 'localhost';
+
+app.get('/', (req, res) => {
+  res.send('<h1> Hello World!</h1>');
+});
+
+app.listen(port, host, () => {
+  console.log(`Server running at http://${host}:${port}/`);
+})
